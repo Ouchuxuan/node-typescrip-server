f08ce500ff431bb9c176afb2ded46870f1c07301
Ouchuxuan <ocx7793709@hotmail.com>
2020-08-24 07:03:07 -0700

    添加日志功能

--- src/config/log-config.ts
+++ src/config/log-config.ts
@@ -1,42 +1,72 @@
+import path from 'path';
+// 日志根目录
+const baseLogPath = path.resolve(__dirname, '../log');
+
+// 错误日志目录
+const errorPath = '/error';
+// 错误日志文件名
+const errorFileName = 'error';
+// 错误日志输出完成路径
+const errorLogPath = baseLogPath + errorPath + '/' + errorFileName;
+
+// 请求日志目录
+const reqPath = '/request';
+// 请求日志文件名
+const reqFileName = 'request';
+// 请求日志输出完整路径
+const reqLogPath = baseLogPath + reqPath + '/' + reqFileName;
+
+// 响应日志目录
+const responsePath = '/response';
+// 响应日志文件名
+const responseFileName = 'response';
+// 响应日志输出完整路径
+
 export default {
+  // 日志格式等设置
   appenders: {
     console: {
       type: 'console'
     },
-    success: {
-      type: 'file',
-      maxLogSize: 5 * 1000 * 1000, // 超过多少(byte)就切割
-      backups: 100, // 保留旧日志数量(default: 5)
-      // compress: true, // 缩成.gz格式
-      filename: 'logs/success.log',
-      pattern: '-yyyy-MM-dd-hh-mm',
-      keepFileExt: true // 切割的日志保留文件扩展名，false(默认):生成类似default.log.1文件;true:生成类似default.1.log
-    },
-    errors: {
+    errorLogger: {
       type: 'dateFile',
-      filename: 'logs/errors.log',
-      // alwaysIncludePattern: true, // 代表在最新的一个日志文件里追加日志(默认是在errors.log文件写入内容)
-      daysToKeep: 2, // 大于0则会删除x天之前的日志
-      pattern: '-yyyy-MM-dd', // yyyy-MM-dd-hh-mm=>每分钟切割一次 yyyy-mm-dd-hh =>每小时
-      keepFileExt: true
-    }
-  },
-  categories: {
-    default: {
-      appenders: ['console'],
-      level: 'all'
+      filename: errorLogPath,
+      pattern: '-yyyy-MM-dd-hh.log',
+      alwaysIncludePattern: true,
+      encoding: 'utf-8',
+      maxLogSize: 1000,
+      numBackups: 3,
+      path: errorPath,
+      layout: {
+        type: 'basic'
+      }
     },
-    success: {
-      appenders: [
-        'success'
-      ],
-      level: 'all'
+    http: {
+      type: 'dateFile',
+      filename: reqLogPath,
+      pattern: '-yyyy-MM-dd-hh.log',
+      alwaysIncludePattern: true,
+      encoding: 'utf-8',
+      maxLogSize: 1000,
+      numBackups: 3,
+      path: reqPath,
+      layout: {
+        type: 'basic'// 'messagePassThrough'
+      }
     },
-    errors: {
-      appenders: [
-        'errors'
-      ],
-      level: 'error'
+    resLogger: {
+      type: 'dateFile',
+      filename: responseLogPath,
+      pattern: '-yyyy-MM-dd-hh.log',
+      alwaysIncludePattern: true,
+      encoding: 'utf-8',
+      maxLogSize: 1000,
+      numBackups: 3,
+      path: responsePath,
+      layout: {
+        type: 'basic'
+      }
     }
   }
+
 }
--- src/middlewares/log.ts
+++ src/middlewares/log.ts
@@ -1,4 +1,10 @@
-import logConfig from '../config/log-config';
-import  * as log4js from 'log4js';
-// 加载配置文件
+// import logConfig from '../config/log-config';
+// import  * as log4js from 'log4js';
+// // 加载配置文件
+// log4js.configure(logConfig);
+
+// let logUtil = {};
+// // 调用预先定义的日志名称
+// const resLogger = log4js.getLogger('resLogger');
+
 
